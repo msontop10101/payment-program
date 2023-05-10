27df2cf79ba1ac56c0bae7dced9286aee1c52c7a
Moyosore <msontop.me@gmail.com>
2023-05-10 14:48:46 +0100

    changes 2 to the table

--- src/App.js
+++ src/App.js
@@ -22,16 +22,16 @@ function App() {
     <div className="App">
       <PaymentForm setSubmittedValues={setSubmittedValues} />
       <div className='flex justify-center'>
-        <div className='my-2 w-[60%]'>
-          <h2 className='font-semibold'>Visualización de la base de datos de pagos</h2>
-          <div className='flex mx-3 mt-4 justify-between font-semibold bg-slate-300 border rounded py-4'>
+        <div className='my-2 w-[60%]' style={{fontFamily:'Bebas Neue' }}>
+          <h2 className='font-normal'>Visualización de la base de datos de pagos</h2>
+          <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
             <div className='w-[45%] text-left'><h2>Nombre</h2></div>
             <div className='w-[45%] text-left'><h2>Pago</h2></div>
           </div>
           {payments.map((user, index) => (
-            <div key={index} className='flex mx-3 mt-1 even:bg-slate-300 justify-between border rounded'>
+            <div key={index} className='flex mx-3 mt-1 even:bg-slate-300 italic px-3 text-xl justify-between border rounded'>
               <div className='w-[45%] text-left py-2 px-1'>{user.name}</div>
-              <div className='w-[45%] text-left py-2 px-1'>${user.payments}</div>
+              <div className='w-[45%] text-left py-2 px-1'><span className='text-[green] text-2xl'>$</span>{user.payments}</div>
             </div>
           ))}
         </div>
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -15,11 +15,11 @@ const PaymentForm = ({setSubmittedValues}) => (
                 }
                 return errors;
             }}
-            onSubmit={(values, { setSubmitting }) => {
+            onSubmit={(values, {resetForm}) => {
                 setTimeout(() => {
                     alert(JSON.stringify(values, null, 2));
-                    setSubmitting(false);
                 }, 400);
+                resetForm()
                 setSubmittedValues(values)
             }}
         >
