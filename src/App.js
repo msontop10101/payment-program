35872f56d9699277a65364634e0e2d6dd3451162
Moyosore <msontop.me@gmail.com>
2023-05-12 21:15:32 +0100

    added blink effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import PaymentForm from './components/Forms';
-import newPaymentSound from './sound/newpayment.mp3'
 import moneySound from './sound/moneysound.mp3'
 
 
@@ -29,6 +28,27 @@ function App() {
     { name: 'Maximiliano Peralta', payments: 430 },
   ])
   const [audio] = useState(new Audio(moneySound));
+  const [newPaymentIndex, setNewPaymentIndex] = useState(null)
+  console.log('payment index: ', newPaymentIndex)
+
+
+  useEffect(() => {
+    if (newPaymentIndex !== null) {
+      let parent = document.getElementById('list-container')
+      let children = parent.children[newPaymentIndex]
+      children.classList.add('new-payment')
+      const timer = setTimeout(() => {
+        setNewPaymentIndex(null);
+      }, 3000); // Adjust the duration (in milliseconds) as per your requirement
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [newPaymentIndex]);
+
+  
+
   const sortedByPayments = [...payments].sort((a, b) => b.payments - a.payments);
 
   const totalPayments = payments.reduce((accumulator, payment) => accumulator + payment.payments, 0);
@@ -39,7 +59,7 @@ function App() {
       const existingPayment = payments.find(payment => payment.name === newData.name);
       if (existingPayment) {
         // update the payments property of the existing object
-        const updatedPayments = payments.map(payment => {
+        const updatedPayments = sortedByPayments.map(payment => {
           if (payment.name === newData.name) {
             return { ...payment, payments: payment.payments + newData.payments };
           } else {
@@ -47,11 +67,18 @@ function App() {
           }
         });
         setPayments(updatedPayments);
+        const sortedUpdatedPayments = [...updatedPayments].sort((a, b) => b.payments - a.payments);
+        const updatedIndex = sortedUpdatedPayments.findIndex(payment => payment.name === newData.name);
+        console.log('Updated_Index:', updatedIndex);
+        setNewPaymentIndex(updatedIndex)
       } else {
         const updatedPayments = [...payments, newData];
         setPayments(updatedPayments);
+        const sortedUpdatedPayments = [...updatedPayments].sort((a, b) => b.payments - a.payments);
+        const newIndex = sortedUpdatedPayments.findIndex(payment => payment.name === newData.name);
+        console.log('Index:', newIndex);
+        setNewPaymentIndex(newIndex)
       }
-
 
       audio.play();
     }
@@ -61,14 +88,14 @@ function App() {
 
 
 
-  
+
 
 
   return (
     <div className="App">
       <PaymentForm setSubmittedValues={setSubmittedValues} />
       <div className='flex justify-center'>
-        
+
         <div className='my-2 w-[60%] background-image' style={{ fontFamily: 'Bebas Neue' }}>
           <h2 className='font-normal'>Visualización de la base de datos de pagos</h2>
           <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
@@ -77,7 +104,8 @@ function App() {
           </div>
           <div id='list-container'>
             {sortedByPayments.map((user, index) => (
-              <div key={index} className='flex mx-3 mt-1 bg-slate-50 opacity-75 even:bg-slate-300 even:opacity-100 italic px-3 text-2xl justify-between border rounded items-center'>
+              
+              <div key={index}  className={`flex mx-3 mt-1 bg-slate-50 opacity-75 even:bg-slate-300 even:opacity-100 italic px-3 text-2xl justify-between border rounded items-center ${newPaymentIndex === index ? 'my-payment' : ''}`}>
                 <div>{index + 1}</div>
                 <div className='w-[45%] text-left py-2 px-1'>{user.name}</div>
                 <div className='w-[45%] text-left py-2 px-1'><span className='text-[green] text-2xl'>$</span> {user.payments}</div>
@@ -89,7 +117,7 @@ function App() {
             <div className='w-[45%] text-left'><h2><span className='text-[green] text-2xl'>$</span> {totalPayments}</h2></div>
           </div>
         </div>
-        
+
       </div>
     </div>
   );
