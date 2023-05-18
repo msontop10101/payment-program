a83b88113a52de570e958e9082b5bb4bfde3811a
Precious Olusola <preciousolusola16@gmail.com>
2023-05-18 18:28:42 +0100

    Updated moeny display width

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,7 +11,6 @@ function Home() {
     const payments = loadData();
     const newPaymentIndex = null
 
-
     // Sort by amount
     const sortedByPayments = [...payments].sort((a, b) => Number(b.amount) - Number(a.amount));
     // Calculate total
@@ -19,28 +18,29 @@ function Home() {
 
     return (
         <div className="App">
-            
             <div className='flex justify-center'>
 
-                <div className='my-2 w-[60%] background-image' style={{ fontFamily: 'Poppins' }}>
-                    <h2 className='font-normal'>Visualización de la base de datos de pagos</h2>
-                    <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
-                        <div className='w-[45%] text-left'><h2>Nombre</h2></div>
-                        <div className='w-[45%] text-left'><h2>VENTAS</h2></div>
-                    </div>
-                    <div id='list-container'>
-                        {sortedByPayments.map((user, index) => (
+                <div className='my-2 w-[90%] background-image' style={{ fontFamily: 'Poppins' }}>
+                    <div className='md:w-[70%] mx-auto'>
+                        <h2 className='font-normal'>Visualización de la base de datos de pagos</h2>
+                        <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
+                            <div className='w-[45%] text-center'><h2>Nombre</h2></div>
+                            <div className='w-[45%] text-center'><h2>VENTAS</h2></div>
+                        </div>
+                        <div id='list-container'>
+                            {sortedByPayments.map((user, index) => (
 
-                            <div key={index} className={`flex mx-3 mt-1 bg-slate-50 opacity-75 even:bg-slate-300 even:opacity-100 italic px-3 text-2xl justify-between border rounded items-center ${newPaymentIndex === index ? 'my-payment' : ''}`}>
-                                <div>{index + 1}</div>
-                                <div className='w-[45%] text-left py-2 px-1'>{user.name}</div>
-                                <div className='w-[45%] text-left py-2 px-1'><span className='text-[green] text-2xl'>$</span> {user.amount}</div>
-                            </div>
-                        ))}
-                    </div>
-                    <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
-                        <div className='w-[45%] text-left'><h2>TOTAL</h2></div>
-                        <div className='w-[45%] text-left'><h2><span className='text-[green] text-2xl'>$</span> {totalPayments}</h2></div>
+                                <div key={index} className={`flex mx-3 mt-1 bg-slate-50 opacity-75 even:bg-slate-300 even:opacity-100 italic px-3 text-2xl justify-between border rounded items-center ${newPaymentIndex === index ? 'my-payment' : ''}`}>
+                                    <div>{index + 1}</div>
+                                    <div className='w-[45%] text-center py-2 px-1'>{user.name}</div>
+                                    <div className='w-[45%] text-center py-2 px-1'><span className='text-[green] text-2xl'>$</span> {user.amount}</div>
+                                </div>
+                            ))}
+                        </div>
+                        <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
+                            <div className='w-[45%] text-center'><h2>TOTAL</h2></div>
+                            <div className='w-[45%] text-center'><h2><span className='text-[green] text-2xl'>$</span> {totalPayments}</h2></div>
+                        </div>
                     </div>
                 </div>
 
