9a841c5aa139afb7fc69f0b9a0415fb0418e1548
Precious Olusola <preciousolusola16@gmail.com>
2023-05-20 01:31:46 +0100

    UI rendering in four columns on large screens

--- src/components/Home.js
+++ src/components/Home.js
@@ -53,31 +53,41 @@ function Home() {
 
 
     return (
-        <div className="App">
-            <div className='flex justify-center'>
+        <div className="App background-image">
+            <div className=' ' style={{ fontFamily: 'Poppins' }}>
 
-                <div className='my-2 w-[90%] background-image' style={{ fontFamily: 'Poppins' }}>
-                    <div className='md:w-[70%] mx-auto'>
-                        {/* <h2 className='font-normal'>Visualización de la base de datos de pagos</h2>*/}
-                        <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
-                            <div className='w-[45%] text-center'><h2>Nombre</h2></div>
-                            <div className='w-[45%] text-center'><h2>VENTAS</h2></div>
-                        </div>
-                        <div id='list-container'>
-                            {sortedByPayments.map((user, index) => (
+                {/* <h2 className='font-normal'>Visualización de la base de datos de pagos</h2> */}
 
-                                <div key={index} className={`flex mx-3 mt-1 bg-slate-50 opacity-75 even:bg-slate-300 even:opacity-100 italic px-3 text-2xl justify-between border rounded items-center ${newPaymentIndex === index ? 'my-payment' : ''}`}>
-                                    <div>{index + 1}</div>
-                                    <div className='w-[45%] text-center py-2 px-1'>{user.name}</div>
-                                    <div className='w-[45%] text-center py-2 px-1'><span className='text-[green] text-2xl'>$</span> {user.amount}</div>
-                                </div>
-                            ))}
-                        </div>
-                        <div className='flex mx-3 mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
-                            <div className='w-[45%] text-center'><h2>TOTAL</h2></div>
-                            <div className='w-[45%] text-center'><h2><span className='text-[green] text-2xl'>$</span> {totalPayments}</h2></div>
-                        </div>
+                <div className='cust-header gap-4'>
+                    <div className='flex mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
+                        <div className='w-[45%] text-center'><h2>Nombre</h2></div>
+                        <div className='w-[45%] text-center'><h2>VENTAS</h2></div>
                     </div>
+
+                    <div className='flex mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
+                        <div className='w-[45%] text-center'><h2>Nombre</h2></div>
+                        <div className='w-[45%] text-center'><h2>VENTAS</h2></div>
+                    </div>
+                </div>
+
+                <div id='list-container'  className='my-2 md:w-[100%] cust-dis'>
+                    {sortedByPayments.map((user, index) => (
+
+                        <div 
+                            key={index} 
+                            className={`flex bg-slate-50 opacity-75 even:bg-slate-300 even:opacity-100 italic px-3 text-2xl justify-between border rounded items-center ${newPaymentIndex === index ? 'my-payment' : ''}`}>
+                            <div>{index + 1}</div>
+                            <div className='w-[45%] text-center py-2 px-1'>{user.name}</div>
+                            <div className='w-[45%] text-center py-2 px-1'><span className='text-[green] text-2xl'>$</span> {user.amount}</div>
+                        </div>
+                    ))}
+                    
+
+                </div>
+
+                <div className='flex cust-footer mt-4 justify-between font-semibold text-2xl px-3 bg-slate-300 border rounded py-4'>
+                    <div className='w-[45%] text-center'><h2>TOTAL</h2></div>
+                    <div className='w-[45%] text-center'><h2><span className='text-[green] text-2xl'>$</span> {totalPayments}</h2></div>
                 </div>
 
             </div>
